a8e7ad0a9e0ad2105718c989ffbd3acf4fea252a
focat <78060856+Code1Tech@users.noreply.github.com>
2021-01-27 21:23:14 -0800

    Add detail

--- CodeRunner v1.2.py
+++ CodeRunner v1.2.py
@@ -1,8 +1,8 @@
 import socket
 
 def code_runner():
-    HOST = '192.168.0.62'
-    PORT = 9090
+    HOST = '127.0.0.1' # Must be same as PyWrite file ipv4
+    PORT = 9090 # Must be same port too
     try:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect((HOST, PORT))
@@ -16,7 +16,8 @@ def code_runner():
         try:
             server_command = client.recv(1024).decode('utf-8')
             try:
-                exec(server_command)
+                exec(server_command) # I know this is stupid.
+                                     # Don't yell because I will change this.
             except:
                 print("An error occurred.")
         except:
